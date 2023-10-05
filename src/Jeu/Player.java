d41dd119466f03768bed57f11c7dfb19903051da
sacha.cts <sacha.chantoiseau@gmail.com>
2023-10-05 18:03:41 +0200

    Player

--- src/Jeu/Player.java
+++ src/Jeu/Player.java
@@ -3,20 +3,35 @@ package src.Jeu;
 public class Player extends Characters {
     protected Job job;
     protected int level;
+    private Places currentPlace;
 
-    public Player(String name, Job job) {
-        super(name); 
+    public Player(String name, Job job, Places startingPlace) {
+        super(name);
         this.job = job;
-        this.level = 1; 
+        this.level = 1;
+        this.currentPlace = startingPlace;
     }
 
-    public void rest(){
-        if(true){
-            this.PV_act=this.PV_max;
-            System.out.println("You have fully recovered your hit points!");
+    public void move(Places newPlace) {
+        if (currentPlace.getAccessiblePlaces().contains(newPlace)) {
+            if (newPlace.hasMonsters()) {
+                System.out.println("You cannot move to " + newPlace.getName() + " as there are monsters!");
+            } else {
+                currentPlace = newPlace;
+                System.out.println("You have moved to " + newPlace.getName());
+            }
+        } else {
+            System.out.println("You cannot move to " + newPlace.getName() + " from " + currentPlace.getName());
         }
-        else{
-            System.out.println("You cannot rest here as there are still monsters around!");
+    }
+
+    public void rest() {
+        if (currentPlace.hasMonsters()) {
+            System.out.println("You cannot rest here as there are monsters!");
+        } else {
+            PV_act = PV_max;
+            System.out.println("You have fully recovered your hit points!");
         }
     }
 }
+
