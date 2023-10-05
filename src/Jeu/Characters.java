5014ce07d517980c76557f808d0b82660156985d
Matthias Carré <lemattheuxbis@gmail.com>
2023-10-05 14:46:44 +0200

    class Characters

---
+++ src/Jeu/Characters.java
@@ -0,0 +1,26 @@
+package Jeu;
+
+public class Characters {
+    protected String name;
+    protected int PV_max;
+    protected int PV_act;
+    protected Places place;
+    protected int Mana_max;
+    protected int Mana_act;
+
+    public Characters(String name){
+        this.Mana_act=0;
+        this.Mana_max=0;
+        this.name=name;
+    }
+
+    public void take_DMG(int n){
+        this.PV_act-=n;
+    }
+    public void hit(Character name,int n){
+        // name.take_DMG(n)
+    }
+    public void talk(String msg){
+        System.out.println(this.name+" dit: "+msg);
+    }
+}
