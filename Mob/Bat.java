2b002c44402c00b32314c0472495bba84c8585f6
jinjamb <95043017+jinjamb@users.noreply.github.com>
2023-10-12 13:29:35 +0200

    Mob finish.

---
+++ Mob/Bat.java
@@ -0,0 +1,14 @@
+package Game.Mob;
+
+public class Bat extends Monsters {
+
+    public Bat(String name) {
+
+        super(name);
+        this.HP_max = 6;
+        this.HP_act = HP_max;
+        this.damage = 5;
+
+    }
+
+}
---
+++ Mob/Gobelin.java
@@ -0,0 +1,14 @@
+package Game.Mob;
+
+public class Gobelin extends Monsters {
+
+    public Gobelin(String name) {
+
+        super(name);
+        this.HP_max = 4;
+        this.HP_act = HP_max;
+        this.damage = 4;
+
+    }
+
+}
