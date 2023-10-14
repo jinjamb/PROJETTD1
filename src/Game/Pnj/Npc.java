8fe3b06268d5bc9a4e4c42992583bcfa6616e869
jinjamb <95043017+jinjamb@users.noreply.github.com>
2023-10-14 10:55:38 +0200

    PNJ

--- src/Game/Pnj/Npc.java
+++ src/Game/Pnj/Npc.java
@@ -1,5 +1,20 @@
 package Game.Pnj;
 
 public class Npc {
-    
+
+    protected String phrase;
+
+    public Npc() {
+        this.phrase = "";
+    }
+
+    public String monsterString() {
+        this.phrase = "Il y a un monstre dans la zone. Au secours !";
+        return phrase;
+    }
+
+    public String passiveString() {
+        this.phrase = "Bonjour combattant.";
+        return phrase;
+    }
 }
